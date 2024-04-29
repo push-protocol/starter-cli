cc831866be028fa200ccdee555a3298d356c80b6
Siddesh <siddeshsankhya@gmail.com>
2024-04-29 14:33:22 +0530

    init cli

---
+++ index.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import {exec} from "child_process";
+import chalk from "chalk";
+
+const templates = [
+  "push-video-starter-with-cra",
+  "push-video-starter-with-nextjs",
+  "push-dapp",
+  "push-website",
+];
+
+const questions = [
+  {
+    type: "list",
+    name: "template",
+    message: "Choose a template:",
+    choices: templates,
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  const selectedTemplate = answers.template;
+
+  exec(
+    `git clone https://github.com/push-protocol/${selectedTemplate}.git`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(chalk.red(`Error: ${error.message}`));
+        return;
+      }
+      console.log(
+        chalk.green(`Cloned ${selectedTemplate} template successfully.`)
+      );
+      console.log("-------------------------------------------------");
+      console.log(chalk.greenBright(`cd ${selectedTemplate} && npm install`));
+      console.log(
+        chalk.greenBright(
+          `npm run ${
+            selectedTemplate === "push-video-starter-with-nextjs"
+              ? "dev"
+              : "start"
+          }`
+        )
+      );
+    }
+  );
+});
